6c5cba9cc5c00444531ed2905b0021245bbb8b8b
Armand Bredean <armand.bredean@gmail.com>
2023-10-04 17:28:21 +0200

    A propos finished

--- src/composant/ColapseBar/index.jsx
+++ src/composant/ColapseBar/index.jsx
@@ -1,19 +1,35 @@
+import React, { useState } from "react";
 import './index.scss';
-import vectorBas from  "../../assets/vectorBas.svg";
+import vectorHaut from  "../../assets/vectorHaut.svg";
+
 function ColapseBar(props) {
   const { colapseTitle, colapsetext } = props;
 
-  return(
+  const [isCollapsed, setIsCollapsed] = useState(true);
+  const [rotationAngle, setRotationAngle] = useState(0); // Initial angle is 0 degrees
+
+  const handleCollapseToggle = () => {
+    setIsCollapsed(!isCollapsed);
+    // Toggle the rotation angle between 0 and 180 degrees
+    setRotationAngle(rotationAngle === 0 ? 180 : 0);
+  };
+
+  return (
     <div className='colapseContainer'>
-      <div className='colapseBar'>
+      <div className='colapseBar' onClick={handleCollapseToggle}>
         <p className='colapseTitle'>{colapseTitle}</p>
-        <img className='colapseArrow' src={vectorBas} alt="vector bas" />
+        <img
+          className='colapseArrow'
+          src={vectorHaut}
+          alt="vector bas"
+          style={{ transform: `rotate(${rotationAngle}deg)` }} // Apply rotation based on the angle
+        />
       </div>
-      <div className='colapseTextContainer'>
+      <div className={`colapseTextContainer ${isCollapsed ? 'collapsed' : ''}`}>
         <p className='colapseText'> {colapsetext}</p>
       </div>
     </div>
-  )
+  );
 }
 
-export default ColapseBar
+export default ColapseBar;
