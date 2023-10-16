b290c18032eab57f40940a264de6f6a7547d6820
Armand Bredean <armand.bredean@gmail.com>
2023-10-16 10:42:59 +0200

    final

--- src/composant/Header/index.jsx
+++ src/composant/Header/index.jsx
@@ -1,17 +1,52 @@
-import Logo from  "../../assets/Logo.png";
+import React, { useState } from 'react';
+import Logo from '../../assets/Logo.png';
 import './index.scss';
-import { NavLink } from "react-router-dom";
+import { NavLink } from 'react-router-dom';
 
 function Header() {
+  // Define a state variable to track the active link
+  const [activeLink, setActiveLink] = useState(null);
+
+  // Function to set the active link
+  const handleLinkClick = (linkName) => {
+    setActiveLink(linkName);
+  };
+
   return (
     <header className="header">
-      <img className="header__logo" src={Logo} alt="logo"></img>
+      <img className="header__logo" src={Logo} alt="logo" />
       <div className="header__navbar">
-        <NavLink to="/Home">ACCUEIL</NavLink>
-        <NavLink to="/APropos">A PROPOS</NavLink>
+        <NavLink
+          to="/Home"
+          className={`header__mobile ${activeLink === 'Accueil' ? 'active' : ''}`}
+          onClick={() => handleLinkClick('Accueil')}
+        >
+          ACCUEIL
+        </NavLink>
+        <NavLink
+          to="/APropos"
+          className={`header__mobile ${activeLink === 'APropos' ? 'active' : ''}`}
+          onClick={() => handleLinkClick('APropos')}
+        >
+          A PROPOS
+        </NavLink>
+        <NavLink
+          to="/Home"
+          className={`header__desktop ${activeLink === 'Accueil' ? 'active' : ''}`}
+          onClick={() => handleLinkClick('Accueil')}
+        >
+          Accueil
+        </NavLink>
+        <NavLink
+          to="/APropos"
+          className={`header__desktop ${activeLink === 'APropos' ? 'active' : ''}`}
+          onClick={() => handleLinkClick('APropos')}
+        >
+          A propos
+        </NavLink>
       </div>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 import Home from './pages/Home';
